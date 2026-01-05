d1328786084e60e5d25e0b15f87242551b98ec0b
Andreea Nedelcu <andreea.nedelcu1204@gmail.com>
2026-01-05 16:46:53 +0200

    feat: humidity live updates

---
+++ firmware/reg_hum.py
@@ -0,0 +1,88 @@
+import network
+import time
+import urequests
+import json
+from machine import ADC, Pin
+import secrets
+
+# --- config
+WIFI_SSID = secrets.WIFI_SSID
+WIFI_PASS = secrets.WIFI_PASS
+
+PROJECT_ID = secrets.PROJECT_ID
+
+USER_UID = secrets.USER_UID
+
+# sensor config (dry and wet vals currently inaccurate)
+SENSOR_PIN = 26        # GP26 (ADC0)
+DRY_VAL = 65535
+WET_VAL = 20000
+
+adc = ADC(Pin(SENSOR_PIN))
+led = Pin("LED", Pin.OUT)
+
+def connect_wifi():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(WIFI_SSID, WIFI_PASS)
+    print("Connecting to WiFi...", end="")
+    while not wlan.isconnected():
+        led.toggle()
+        time.sleep(0.2)
+        print(".", end="")
+    print("\nConnected! IP:", wlan.ifconfig()[0])
+    led.on()
+
+def get_humidity_percent():
+    raw = adc.read_u16()
+
+    if raw >= DRY_VAL:
+        return 0
+    elif raw <= WET_VAL:
+        return 100
+    else:
+        percent = (DRY_VAL - raw) / (DRY_VAL - WET_VAL) * 100
+        return round(percent, 1) # 1. decimal
+
+def update_firestore(humid_val):
+    url = f"https://firestore.googleapis.com/v1/projects/{PROJECT_ID}/databases/(default)/documents/users/{USER_UID}?updateMask.fieldPaths=greenhouse.Humid.value"
+    
+    payload = {
+        "fields": {
+            "greenhouse": {
+                "mapValue": {
+                    "fields": {
+                        "Humid": {
+                            "mapValue": {
+                                "fields": {
+                                    "value": { "doubleValue": humid_val }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    try:
+        # patch to update existing document
+        response = urequests.patch(url, json=payload)
+        if response.status_code == 200:
+            print(f"Updated DB: {humid_val}%")
+        else:
+            print(f"Error {response.status_code}: {response.text}")
+        response.close()
+    except Exception as e:
+        print("Network Error:", e)
+
+# -- main loop
+connect_wifi()
+
+print("Starting Sensor Loop...")
+while True:
+    current_humid = get_humidity_percent()
+    
+    update_firestore(current_humid)
+    
+    time.sleep(3)
